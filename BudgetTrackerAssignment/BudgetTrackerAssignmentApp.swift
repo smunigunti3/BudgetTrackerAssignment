f4272cde2f96c44e973efe4f9b64fced6d5f938b
royankit11 <royankit11@gmail.com>
2025-08-22 14:47:40 -0400

    Initial Commit

---
+++ BudgetTrackerAssignment/BudgetTrackerAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerAssignmentApp.swift
+//  BudgetTrackerAssignment
+//
+//  Created by Rik Roy on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetTrackerAssignmentTests/BudgetTrackerAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerAssignmentTests.swift
+//  BudgetTrackerAssignmentTests
+//
+//  Created by Rik Roy on 8/22/25.
+//
+
+import Testing
+@testable import BudgetTrackerAssignment
+
+struct BudgetTrackerAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
